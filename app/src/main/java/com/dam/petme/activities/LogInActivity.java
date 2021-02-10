0280dab4c04800d01e939976681cc36133d6065b
Santiago Cobelli <cobellisantiago@gmail.com>
2021-02-10 19:43:19 -0300

    Add Firebase Realtime Database config and Firebase Auth config Add Feature to create account Add Feature to login

--- app/src/main/java/com/dam/petme/activities/LogInActivity.java
+++ app/src/main/java/com/dam/petme/activities/LogInActivity.java
@@ -1,19 +1,26 @@
 package com.dam.petme.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.dam.petme.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
@@ -24,11 +31,14 @@ public class LogInActivity extends AppCompatActivity {
     Button signInButton;
     TextView signUpTextView;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        mAuth = FirebaseAuth.getInstance();
 
         emailLogInTextInputLayout = findViewById(R.id.emailLogInTextInputLayout);
         passwordLogInTextInputLayout = findViewById(R.id.passwordLogInTextInputLayout);
@@ -45,8 +55,8 @@ public class LogInActivity extends AppCompatActivity {
                 email = emailLogInTextInputLayout.getEditText().getText().toString();
                 password = passwordLogInTextInputLayout.getEditText().getText().toString();
 
-                if(validUser()) {
-                    Toast.makeText(LogInActivity.this,"Ingresa",Toast.LENGTH_SHORT).show();
+                if (validUser()) {
+                    Toast.makeText(LogInActivity.this, "Ingresa", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -61,18 +71,30 @@ public class LogInActivity extends AppCompatActivity {
 
     }
 
-    public boolean validUser(){
-        if(validateAllMandatoryFields()) {
-            if(userExists()) {
-                logInError.setVisibility(View.GONE);
-                return true;
-            }
-            else {
-                logInError.setVisibility(View.VISIBLE);
-                return false;
-            }
-        }
-        else {
+    public boolean validUser() {
+        if (validateAllMandatoryFields()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("FB/Auth", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(LogInActivity.this, HomeActivity.class);
+                                startActivity(intent);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("FB/Auth", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+            return true;
+        } else {
             logInError.setVisibility(View.GONE);
             return false;
         }
